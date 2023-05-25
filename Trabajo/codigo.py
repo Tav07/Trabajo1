e481ec79aa76b2842ad79da1e73a85dcf2026987
Tav07 <gmendez650@soyudemedellin.edu.co>
2023-05-24 21:10:24 -0500

    CODIGO ACTUALIZADO

--- Trabajo/codigo.py
+++ Trabajo/codigo.py
@@ -1,85 +1,89 @@
-tarifa_base = 2000
-total_espacios = 100
+import tkinter as tk
+from datetime import datetime
 
+def calcular_tarifa(tipo_vehiculo, tiempo):
+    if tipo_vehiculo == 'carro':
+        tarifa_hora = 4700
+        tarifa_dia = 19000
+        tarifa_mes = 276000
+    elif tipo_vehiculo == 'moto':
+        tarifa_hora = 2100
+        tarifa_dia = 9500
+        tarifa_mes = 89500
 
-class Cliente:
-    def __init__(self, nombre: str, placa: str):
-        self.nombre = nombre
-        self.placa = placa
-        self.tiempo = 0
+    dias = tiempo.days
+    horas = tiempo.seconds // 3600
+    minutos = (tiempo.seconds % 3600) // 60
 
-    def calcular_costo(self) -> int:
-        costo = self.tiempo * tarifa_base
-        return costo
+    # Si el tiempo no supera una hora, se cobra la tarifa completa de una hora
+    if dias == 0 and horas == 0 and minutos <= 60:
+        return tarifa_hora
 
+    total = (dias * tarifa_dia) + (horas * tarifa_hora)
+    if dias >= 31:
+        total = tarifa_mes
 
-class Administrador:
-    def __init__(self):
-        self.ingresos = 0
-        self.egresos = 0
-        self.espacios_disponibles = total_espacios
-        self.espacios_ocupados = 0
+    return total
 
-    def registrar_ingreso(self, cliente: Cliente) -> None:
-        self.ingresos += cliente.calcular_costo()
-        self.espacios_disponibles -= 1
-        self.espacios_ocupados += 1
+def registrar_entrada():
+    placa = placa_entry.get()
+    tipo_vehiculo = tipo_vehiculo_var.get()
+    hora_entrada = datetime.now()
+    vehiculos_registrados[placa] = hora_entrada
+    entrada_label.config(text=f"Registro de entrada exitoso. Hora de entrada: {hora_entrada}")
 
-    def registrar_egreso(self, cliente: Cliente) -> None:
-        self.egresos += cliente.calcular_costo()
-        self.espacios_disponibles += 1
-        self.espacios_ocupados -= 1
+def generar_factura():
+    placa = placa_entry.get()
+    tipo_vehiculo = tipo_vehiculo_var.get()
+    hora_salida = datetime.now()
+    if placa in vehiculos_registrados:
+        hora_entrada = vehiculos_registrados[placa]
+        tiempo_estacionado = hora_salida - hora_entrada
+        tarifa_total = calcular_tarifa(tipo_vehiculo, tiempo_estacionado)
+        factura = f"--- FACTURA ---\n"
+        factura += f"Placa: {placa}\n"
+        factura += f"Tipo de vehículo: {tipo_vehiculo}\n"
+        factura += f"Hora de entrada: {hora_entrada}\n"
+        factura += f"Hora de salida: {hora_salida}\n"
+        factura += f"Tiempo estacionado: {tiempo_estacionado}\n"
+        factura += f"Tarifa total: {tarifa_total} pesos\n"
+        factura_text.configure(state='normal')
+        factura_text.delete("1.0", tk.END)
+        factura_text.insert(tk.END, factura)
+        factura_text.configure(state='disabled')
+        del vehiculos_registrados[placa]
+    else:
+        factura_text.configure(state='normal')
+        factura_text.delete("1.0", tk.END)
+        factura_text.insert(tk.END, "El vehículo no ha sido registrado.")
+        factura_text.configure(state='disabled')
 
-    def generar_reporte(self) -> str:
-        reporte = f"Ingresos: {self.ingresos}\nEgresos: {self.egresos}\nEspacios disponibles: {self.espacios_disponibles}"
-        return reporte
+vehiculos_registrados = {}
 
+window = tk.Tk()
+window.title("Sistema de Facturación de Estacionamiento")
 
-class Parqueadero:
-    def __init__(self):
-        self.administrador = Administrador()
-        self.clientes = []
+# Crear los elementos de la interfaz
+placa_label = tk.Label(window, text="Placa del vehículo:")
+placa_entry = tk.Entry(window)
+tipo_vehiculo_label = tk.Label(window, text="Tipo de vehículo:")
+tipo_vehiculo_var = tk.StringVar(window)
+tipo_vehiculo_var.set("carro")
+tipo_vehiculo_optionmenu = tk.OptionMenu(window, tipo_vehiculo_var, "carro", "moto")
+registrar_entrada_btn = tk.Button(window, text="Registrar Entrada", command=registrar_entrada)
+generar_factura_btn = tk.Button(window, text="Generar Factura", command=generar_factura)
+entrada_label = tk.Label(window, text="")
+factura_text = tk.Text(window, height=10, width=50)
+factura_text.configure(state='disabled')
 
-    def ingresar_cliente(self, nombre: str, placa: str) -> None:
-        cliente = Cliente(nombre, placa)
-        self.clientes.append(cliente)
-        self.administrador.registrar_ingreso(cliente)
+# Posicionar los elementos en la interfaz
+placa_label.pack()
+placa_entry.pack()
+tipo_vehiculo_label.pack()
+tipo_vehiculo_optionmenu.pack()
+registrar_entrada_btn.pack()
+entrada_label.pack()
+generar_factura_btn.pack()
+factura_text.pack()
 
-    def retirar_cliente(self, placa: str) -> None:
-        for cliente in self.clientes:
-            if cliente.placa == placa:
-                self.clientes.remove(cliente)
-                self.administrador.registrar_egreso(cliente)
-
-    def generar_reporte(self) -> str:
-        reporte = self.administrador.generar_reporte()
-        return reporte
-
-
-parqueadero = Parqueadero()
-
-while True:
-    print("\n1. Registrar Ingreso")
-    print("2. Calcular Costo")
-    print("3. Registrar Egreso")
-    opcion = int(input("Ingrese la opción deseada: "))
-
-    if opcion == 1:
-        nombre = input("Ingrese el nombre del cliente: ")
-        placa = input("Ingrese la placa del vehículo: ")
-        parqueadero.ingresar_cliente(nombre, placa)
-        print("Ingreso registrado exitosamente.")
-
-    elif opcion == 2:
-        placa = input("Ingrese la placa del vehículo: ")
-        tiempo = int(input("Ingrese el tiempo de estacionamiento en horas: "))
-        for cliente in parqueadero.clientes:
-            if cliente.placa == placa:
-                cliente.tiempo = tiempo
-                costo = cliente.calcular_costo()
-                print("Pague", costo)
-
-    elif opcion == 3:
-        placa = input("Ingrese la placa del vehículo: ")
-        parqueadero.retirar_cliente(placa)
-        print("Egreso registrado exitosamente.")
+window.mainloop()
